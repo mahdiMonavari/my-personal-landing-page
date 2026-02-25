bcfa8ae51c46a9ada0ba38cce9c143abf6ea544e
mahdi Developer <mahdi.monavari1381@gmail.com>
2026-02-25 19:56:57 +0330

    feat : hero text coding

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 import TopBar from "./Components/TopBar";
 
 function App() {
+  const [ability, setAbiliti] = useState(["Front-end", "Back-end"]);
   return (
     <div className="bg-primary">
       <TopBar />
-      <div className="h-screen container">
-        <div className="bg-custom-orange h-full"></div>
+      <div className="h-screen container flex bg-custom-orange items-center">
+        <div className="">
+          <h2
+            className="text-4xl bg-gradient-to-r from-purple-700 via-blue-500 to-purple-700 font-bold
+        bg-clip-text text-transparent"
+          >
+            Hi, Im Mahdi Monavari
+          </h2>
+
+          <h2 className="text-white text-start text-4xl">
+            <span
+              className="bg-gradient-to-r from-purple-700 via-blue-500 to-purple-700 font-bold
+        bg-clip-text text-transparent"
+            >
+              Im{" "}
+            </span>
+            <span
+              className="bg-gradient-to-r from-pink-700 via-red-200 to-pink-700 font-bold 
+              inline-block origin-bottom-left rotate-45 bg-clip-text text-transparent"
+            >
+              {ability[0]}
+            </span>
+          </h2>
+        </div>
       </div>
     </div>
   );
